f705a728ac8850bdb0e3f3fe3c5764d7d1272ac9
qimijoy <qimijoy@yandex.ru>
2025-03-03 10:33:09 +0300

    Updates

--- gulp-utils/tasks/clean.js
+++
@@ -1,6 +0,0 @@
-import paths from '../paths.js';
-import { deleteAsync } from 'del';
-
-export default () => {
-	return deleteAsync(paths.clean);
-};
--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	document.body.classList.add('loading');
-});
-
-window.addEventListener('load', () => {
-	document.querySelector('.preloader').classList.add('preloader_hidden');
-	document.body.classList.remove('loading');
-});
